3a7c69957f5c6e5c73537f703a27dfbbabfbb796
Your Name <you@example.com>
2021-05-05 20:44:13 +0800

    IBI1_Group_ICA

--- IBI1_group_ICA/Task2.py
+++ IBI1_group_ICA/Task2.py
@@ -6,31 +6,31 @@ def ORF_length_calculator():
         mRNA_seq = input('Please input a mRNA sequence: ')
 
 	# Only exist one start codon AUG
-	start_codon = 'ATG'
+        start_codon = 'ATG'
 	# Choose one of three stop codon 
-	stop_codon = input('Please input the stop codon: ')
+        stop_codon = input('Please input the stop codon: ')
 
 	#transfer all 'U' to 'T'
-	stop_codon = re.sub(r'U', 'T', stop_codon)
-	mRNA_seq = re.sub(r'U', 'T', mRNA_seq)
+        stop_codon = re.sub(r'U', 'T', stop_codon)
+        mRNA_seq = re.sub(r'U', 'T', mRNA_seq)
 	#calculate the length
-	length = len(mRNA_seq)
+        length = len(mRNA_seq)
 	
 	#split the seq. by stop codon
 	#form a list
-	cut = re.split(stop_codon, mRNA_seq)
-	goal = []
+        cut = re.split(stop_codon, mRNA_seq)
+        goal = []
 	#Find the seq. start from 'AUG'
-	for i in cut:
-		goal1 = re.findall('(?<=ATG).*$', i)
-		goal.append(goal1)
-	count = 0
+        for i in cut:
+                goal1 = re.findall('(?<=ATG).*$', i)
+                goal.append(goal1)
+        count = 0
 	#count each seq. length
-	for i in goal:
-		count = count + len(i) + 6
+        for i in goal:
+                count = count + len(i) + 6
 	#calculate the percantage
-	result = (count / length) * 100
-	print('The percentage of ORF length is', result, '%.')
+        result = (count / length) * 100
+        print('The percentage of ORF length is', result, '%.')
 ORF_length_calculator()
 
 # TTTATGCCCCCCCCCTGAAAAATGCCCCCCCCCTGAATGCCCCCCCCCTGACCCCC

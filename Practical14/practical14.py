15ffdebea9a5c02707da7782becc703871139e9c
Yuefeng Wu <yuefeng.20@intl.zju.edu.cn>
2021-05-18 09:13:05 +0800

    add persudocode

--- Practical14/practical14.py
+++ Practical14/practical14.py
@@ -4,91 +4,105 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+#remove all the duplicate elements from the list
+def similar(list):
+    list1=[]
+    list2=[]
+    list1 = list
+    for i in list1:
+        if i not in list2:
+            list2.append(i)
+
+    return list2
+
 doc= parse('go_obo.xml')
 # get rootnode
 root=doc.documentElement
 terms=root.getElementsByTagName('term')
 dic_def = {}
-#store the relationship between id and its description
 dic_a = {}
-#store the relationship between id and is_a
 for term in terms:
+    #split the xml into categories by terms
     id = term.getElementsByTagName("id")[0]
     id_inf=id.childNodes[0].data
-    # get the id in each terms
-    #print(id.childNodes[0].data) # !!!!write for test use only
+    #get the id content
+    #print(id.childNodes[0].data) !!!for test use only
     description = term.getElementsByTagName("defstr")[0]
-    #get the corresponding description for each id
-    #print(description.childNodes[0].data) # !!!!write for test use only
+    #print(description.childNodes[0].data)
     des_inf =description.childNodes[0].data
+    #get the description conntent for each corresponding id
     dic_def[id_inf]=des_inf
-    # build a dictionary for the relationship between id and its description
+    #form a list, key is the id, value is description
     is_a = term.getElementsByTagName("is_a")
-    # get the corresponding is_a for each id
     list1=[]
-    #multiple is_a existed, use a list to store them
+    # put all corresponding is_a into a list
     for i in is_a:
-        #print("is_a",i.childNodes[0].data)
+        #print("is_a",i.childNodes[0].data)!!!for test use only
         list1.append(i.childNodes[0].data)
     dic_a[id_inf]=list1
-    # build a dictionary for the relationship between id and its is_a
-
-#print(dic_a) # !!!!write for test use only
+    #form a list, key is the id, value is is_a list
+# print(dic_a)!!!for test use only
 
 key = input('please input the keyword')
-# the DNA, RNA, and protein have been put and the outcomes were taken
-#oligosaccharide is the fourth macromolecule
 dic_temp = {}
+#go through the dictionary to screen out ids we want
 for i in dic_def.keys():
     if re.search(key, dic_def[i]):
-        #test if the 'DNA' or other intended keynames existed
         value = dic_def[i]
         dic_temp[i] = value
-array = dic_temp.keys()
-#A list store all the key related ids
-#print(array) # !!!!write for test use only
-sum_array =[]
+array = dic_temp.keys() #contain the ids we want
+#print(array) !!!for test use only
+sum_array =[] #used to store the total fathernode number for each selected id
+
 for id_name in array:
-    #take one ids at a time
-    #print('id_name',id_name)  # !!!!write for test use only
+    #grab a id at a time
+    # print('id_name',id_name)!!!for test use only
     count = 0
     fathernode = []
+    all_node=[]
+    #get id's fathernode put them in a new list
     for k in dic_a:
         if id_name in dic_a[k]:
-            #test whether fathernodes exist
-            count = count + 1
             fathernode.append(k)
-            #store its fathernodes in a list
+            fathernode=similar(fathernode)
+            #only put new fathernode into the list
     fathernode_temp = fathernode[:]
-    # print(fathernode_temp) # !!!!write for test use only
+    count = len(fathernode_temp)
+    #print(fathernode_temp)!!!for test use only
+    all_node = fathernode_temp
     sum_up = count
+    #if on fathernode existed quit the test for this id
+    #while father node still can be found, keep searching
     while sum_up != 0 :
         sum_up = 0
         fathernode = []
         for j in fathernode_temp:
             for q in dic_a:
                 if j in dic_a[q]:
-                    count = count + 1
                     sum_up = sum_up + 1
-                    #while fathernode existed the sum_up is greater than 0, the while loop will keep running
+                    #if no fathernode, the sum_up will be 0
                     fathernode.append(q)
-                    #print(q) # !!!!write for test use only
+                    #print(q) !!!for test use only
         fathernode_temp = fathernode[:]
-        #print(fathernode_temp) # !!!!write for test use only
-    sum_array.append(count)
-    #store the node number for each id
+        fathernode_temp = similar(fathernode_temp)
+        all_node = all_node + fathernode_temp        
+    all_node = similar(all_node)
+    #only count different nodes for each seleced id
+    sum_array.append(len(all_node))
 
-
+#sum up the fathernode number for all selected ids
 count_add = 0
-#get the total number
 for i in sum_array:
     count_add = count_add + i
 print(count_add)
 
 
+#the DNA, RNA, protein and oligosaccharide have been put into test
+#the results are shown below
+#it is a time consuming program, please wait
 
-labels = ['DNA', 'RNA', 'Protein','Oligosaccharide']
-X = [270722, 34097, 684528,728]
+labels = ['DNA', 'RNA', 'protein','oligosaccharide']
+X = [12707, 20383, 77691,580]
 
 fig = plt.figure()
 plt.pie(X, labels=labels, autopct='%1.2f%%')

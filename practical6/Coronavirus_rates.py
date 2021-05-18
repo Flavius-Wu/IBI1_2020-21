133a806f253a95dff88809fbd27fa20938cae5a9
Yuefeng Wu <yuefeng.20@intl.zju.edu.cn>
2021-05-18 09:40:52 +0800

    update practical6

--- practical6/Coronavirus_rates.py
+++ practical6/Coronavirus_rates.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt # to plot figure
 corona={} #make an empty list
 corona={'USA':30139457,'Brazil':11525477,'India':11409831,'Russia':4409438,'UK':5263527} #add the list value
-Keyname=list(corona.keys()) #get all keys
+Keyname=list(corona.keys()) #get all keys # keys are country names
 coronavalue=list(corona.values()) #get all values
 L=[]
 total = 0
@@ -20,4 +20,5 @@ print(dictionary)
 fig, ax1 = plt.subplots() #define row number and line number
 ax1.pie(coronavalue, labels=Keyname, autopct='%.1f%%', pctdistance=0.7, shadow=True, startangle=90) #input value and define style
 ax1.axis('equal')
+ax1.set_title('COVID infected frequency distribution',fontsize=12,color='r')
 plt.show()
